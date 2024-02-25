795fd05700d685632877299252f8697139250dd4
ACER <ACER@LAPTOP-SH69EPPJ>
2024-02-25 19:20:01 +0200

    DbSets added to the applications DbContext

---
+++ FitnessTrackingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using FitnessTrackingSystem.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTrackingSystem.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Challenge> Challenges { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Goal> Goals { get; set; }  
+        public DbSet<Member> Members { get; set; }
+    }
+}
