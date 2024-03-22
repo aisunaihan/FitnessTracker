270ca42d1c8fd152c518186c6e74e7275459a2a9
ACER <ACER@LAPTOP-SH69EPPJ>
2024-03-22 22:32:48 +0200

    Become Trainer method implemented

--- FitnessTrackingSystem.Core/Models/Trainer/BecomeTrainerFormModel.cs
+++ FitnessTrackingSystem.Core/Models/Trainer/BecomeTrainerFormModel.cs
@@ -1,6 +1,35 @@
-﻿namespace FitnessTrackingSystem.Core.Models.Trainer
+﻿using FitnessTrackingSystem.Infrastructure.Data.DataConstants;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitnessTrackingSystem.Core.Models.Trainer
 {
     public class BecomeTrainerFormModel
     {
+        [Required(ErrorMessage=ErrorMessages.RequiredMessage)]
+        [StringLength(
+            TrainerDataConstants.MaxTrainerFullNameLength,
+            MinimumLength = TrainerDataConstants.MinTrainerFullNameLength,
+            ErrorMessage=ErrorMessages.LengthMessage)]
+        [Display(Name="Full name")]
+        public string FullName { get; set; } =string.Empty;
+
+        [Required(ErrorMessage = ErrorMessages.RequiredMessage)]
+        [StringLength(
+            TrainerDataConstants.MaxTrainerPhoneNumLength,
+            MinimumLength = TrainerDataConstants.MinTrainerPhoneNumLength,
+            ErrorMessage = ErrorMessages.LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [StringLength(
+            TrainerDataConstants.MaxTrainerBioLength,
+            MinimumLength = TrainerDataConstants.MinTrainerBioLength,
+            ErrorMessage = ErrorMessages.LengthMessage)]
+        [Display(Name = "Biography")]
+        public string Biography {  get; set; } = string.Empty;  
+        public string ImageUrl { get; set; } = string.Empty;
+
     }
 }
